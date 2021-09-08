f2248e7bf5dd2b44a320732617b955b5dd6fd886
Vladimir Shatskiy <vovaysach@gmail.com>
2021-09-08 13:30:18 +0300

    add all apis

--- apis.py
+++ apis.py
@@ -1,6 +1,7 @@
 from flask import current_app
 import json
 import fnmatch
+from flask.typing import StatusCode
 from requests import request
 import config
 import dropbox
@@ -16,25 +17,34 @@ def weather_by_city(city_name):
         "num_of_days": 1,
         "lang": "ru"
     }
-
+    
     try:
-        result = requests.get(weather_url, params=params)
+        result = requests.get(weather_url, params=params, timeout=1)
         weather = result.json()
         result.raise_for_status()
-    except (requests.RequestException, ValueError) as err:
-        print(f"сервер погоды недоступен! ошибка: {err}")
-        return False
+    except ValueError as errv:
+        return {"error": "value error: " + str(errv)}, 500
+    except requests.exceptions.HTTPError as errh:
+        print(type(errh))
+        return {"error": "Http Error: " + str(errh)}, result.status_code
+    except requests.exceptions.ConnectionError as errc:
+        return {"error": "Error Connecting:" + str(errc)}, 404
+    except requests.exceptions.Timeout as errt:
+        return {"error": "Timeout Error:" + str(errt)}, 408
+    except requests.exceptions.RequestException as err:
+        return {"error": "OOps: Something Else" + str(err)}, result.status_code
+    
 
     if 'data' in weather:
         if 'current_condition' in weather['data']:
             try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
-    return False
+                return weather['data']['current_condition'][0], 200
+            except(IndexError, TypeError) as err:
+                return {"error": "Data error:" + err}, 404
+    return {"error": "Data is empty!"}, 404
 
 
-def get_book(book_to_find="Ложная слепота"):
+def get_book(book_to_find):
     params = {"q": book_to_find, "maxResults": 3}
     try: 
         response = requests.get(config.GOOGLE_BOOKS_API, params=params)
@@ -42,17 +52,22 @@ def get_book(book_to_find="Ложная слепота"):
         books = response.json()
     except (requests.RequestException, ValueError) as err:
         print(f"сервер поиска книг недоступен! Ошибка:{err}")
-        return False
-    if books['items']:
-        for book in books["items"]:
-            volume = book["volumeInfo"]
-            title = volume["title"]
-            published = volume.get("publishedDate", "год издания неизвестен")
-            description = volume.get("description", "описание отсутствует")
-            print(f"{title} ({published}) | {description}")
-    else:
-        return False
+        return {"error": err}, 404
+    # if books['items']:
+    #     for book in books["items"]:
+    #         volume = book["volumeInfo"]
+    #         title = volume["title"]
+    #         published = volume.get("publishedDate", "год издания неизвестен")
+    #         description = volume.get("description", "описание отсутствует")
+    #         print(f"{title} ({published}) | {description}")
 
+    # else:
+    #     return {"error": "data is empty"}, 404 
+    # print(books)
+    if books.get("items", False):
+        return books['items'], 200
+    else:
+        return {"error": "data is empty"}, 404 
 
 def dropbox_files(mask='*'):
     
@@ -71,7 +86,7 @@ def dropbox_files(mask='*'):
             response = requests.post('https://api.dropbox.com/oauth2/token', data=data, auth=(config.DROPBOX_APP_KEY, config.DROPBOX_APP_SECRET))
             tokens = response.json()
             access_token = tokens["access_token"]
-            write_read_tokens(access_token, refresh_token)
+            write_read_write_tokens(access_token, refresh_token)
             print(response.json())
         # print(dbx.files_list_folder(path=''))
         token_str = f'Bearer {access_token}'
@@ -81,8 +96,8 @@ def dropbox_files(mask='*'):
 
         data = '{"query": "foo"}'
 
-        # response = requests.post('https://api.dropboxapi.com/2/check/user', headers=headers, data=data)
-        # print(response.json())
+        response = requests.post('https://api.dropboxapi.com/2/check/user', headers=headers, data=data)
+        print(response.json())
         # token_str = f'Bearer {access_token}'
         headers = {'Authorization': token_str}
         data = '{"path": ""}'
@@ -134,22 +149,24 @@ def dropbox_files(mask='*'):
         files = response.json()
         files = files['entries']
         # print(entries)
+        list_files = []
         for file in files:
             mask = '*'
             if fnmatch.fnmatch(file['name'], mask):
-                print(file['name'])
+                list_files.append(file['name'])
+        return list_files, 200
        
 
-def write_read_tokens(access_token=None, refresh_token=None):  
-    if (access_token is None ) and refresh_token is  None:
+def write_read_write_tokens(access_token=None, refresh_token=None):  
+    if access_token is None  and refresh_token is  None:
         method = 'r'
     else:
         method = 'w'
     with open(config.DB_TK_JSON, method, encoding='utf-8') as f:
         if method == 'r':    
             tokens = json.load(f)
-            access_token = tokens["access_token"]
-            refresh_token = tokens["refresh_token"]    
+            access_token = tokens.get("access_token", False)
+            refresh_token = tokens.get("refresh_token", False)    
             return access_token, refresh_token
         else:
             tokens = {"access_token": access_token, 
--- server.py
+++ server.py
@@ -1,14 +1,29 @@
 import json
+from distutils.util import strtobool
 from logging import error
 from flask import Flask, current_app, request, jsonify
-from apis import weather_by_city
+from apis import weather_by_city, get_book, dropbox_files
 from flask_restful import Api, Resource
+
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 api = Api(app)
 # @app.route("/", methods=["GET"])
 # def get_data_api():
 #     json 
+def check_args(args_data):
+    args_data = dict(args_data)
+    for key, value in args_data.items():
+        if key not in ['weather', 'book_find', 'dropbox_files', 'city', 'book', 'mask']:
+            return False, None
+        if key in ['weather', 'book_find', 'dropbox_files']:
+            try:
+                print(type(value))
+                args_data[key] = strtobool(value.lower()) 
+            except ValueError:
+                return False, None
+    return True, args_data
+    
 
 class Data_Api(Resource):
     def post():
@@ -17,16 +32,45 @@ class Data_Api(Resource):
 
     def get(self):
         # json_data = request.get_json()
+        ## получаем данные погоды
+        # print(request)
         args_data = request.args
-        city = args_data.get('city', 0)
-        if city == 0:
-            city = app.config["WEATHER_DEFAULT_CITY"]
-        # else:
-        #     city = json_data['city']
-        weather =  weather_by_city(city)
-        if weather:
-            return weather, 200
-        return "error", 404
+        check, args_data = check_args(args_data) 
+        if not check:
+            return {"error": "wrong arguments!"}, 400
+        weather_on = args_data.get('weather', True)
+        book_on = args_data.get('book_find', True) 
+        dropbox_on = args_data.get('drobpox_files', True)
+       
+        
+        if weather_on:
+            city = args_data.get('city', app.config["WEATHER_DEFAULT_CITY"])
+            weather, status_code_weather = weather_by_city(city)
+            responce = {"weather": {"status_code": status_code_weather,
+                                    "data": weather 
+                                    }
+                        }
+            if status_code_weather == 200:
+                responce["weather"]['city'] = city
+        else: 
+            responce = {}
+        ##поиск по книгам
+        if book_on:
+            book = args_data.get('book', app.config['DEFAULT_BOOK'])
+            books_list, status_code_book = get_book(book)
+            responce['book_find'] = {"data": books_list, 
+                                "status_code": status_code_book
+                                }   
+        ## поиск по файлам
+        if dropbox_on:
+            mask = args_data.get("mask", "*")   
+            files_list, status_code_db = dropbox_files(mask)
+            responce['dropbox_files'] = {"data": files_list,
+                                         "status_code": status_code_db
+                                         }
+        if not responce:
+            return {"error": "all services not available"}, 404
+        return responce, 200
 
     def delete():
         pass
